e8f1f421c9f2cff30ceab0567b6f62165927ad23
Shaurya21032014 <haritika.arora@gmail.com>
2025-11-23 15:20:30 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,15 @@
+marks = (78,92,81,65,72,83)
+print(marks)
+
+
+marks2= (98, "codingal", True)
+print(marks2)
+
+marks3=(45,90,(78,47,59,62),[87,88,89])
+print(marks3)
+
+print(marks3[2])
+print(marks3[3][2])
+
+for i in  marks :
+    print("hello i  got", i)
